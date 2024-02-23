a169277ae73c1a0d25ea71c014046b6e4e34abcb
dakohhh <wiizzydreadmill@gmail.com>
2024-02-23 15:23:10 -0800

    update: switched to async task for signup route

--- routers/student.py
+++ routers/student.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Request, status
 from authentication.auth import Auth
 from database.schema import Department, Student
 from client.response import CustomResponse
+from repository.department import DepartmentRepository
 from repository.student import StudentRepository
 from utils.query import LoginSchema, SignupSchema
 from exceptions.custom_exception import BadRequestException
@@ -14,25 +15,29 @@ router = APIRouter(tags=["Student"], prefix="/student")
 auth = Auth()
 
 
-
-
 @router.post("/signup")
 async def signup_student(request: Request, signup_input: SignupSchema):
 
+    tasks = [
+        StudentRepository.does_student_email_exits(signup_input.email),
+        await DepartmentRepository.does_department_exist(signup_input.department),
+    ]
 
-    
+    (
+        does_student_email_exits,
+        does_department_exist,
+    ) = await asyncio.gather(*tasks)
 
-    if await StudentRepository.does_student_email_exits(signup_input.email):
+    if await does_student_email_exits:
 
         raise BadRequestException("email already exists")
-        
+
+    if not does_department_exist:
+
+        raise BadRequestException("department does not already exists")
+    
 
     
 
-    context = {"token": "wd"}
 
-    return CustomResponse("login user successfully", data=context)
-
-
-
-
+    return CustomResponse("login user successfully")
